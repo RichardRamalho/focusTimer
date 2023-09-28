cd8f4c1f7cabde438461bc545654e067a227f516
RichardRamalho <128335563+RichardRamalho@users.noreply.github.com>
2023-09-28 20:22:20 -0300

    Add files via upload

---
+++ Js/FocusTimer/sounds.js
@@ -0,0 +1,6 @@
+export const buttonPressAudio = new Audio('./src/sounds/assets_sounds_audios_button-press.wav')
+
+export const kitchenTimer = new Audio('./src/sounds/assets_sounds_kitchentimer.wav')
+
+export const rain = new Audio('./src/sounds/Chuva.wav') 
+rain.loop = true
